00d361ee4b361e244ada3318b097fd42d993c0b7
adamz2303 <adamz2303@gmail.com>
2025-11-09 16:02:46 +0100

    Delete symbioza-live directory

--- symbioza-live/js/lights-animation.js
+++
@@ -1,7 +0,0 @@
-// lights-animation.js
-document.addEventListener('DOMContentLoaded', () => {
-    const lights = document.querySelectorAll('.light');
-    lights.forEach((light, i) => {
-        light.style.animationDelay = `${i * 0.66}s`;
-    });
-});
